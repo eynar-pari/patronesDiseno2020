b59709117ce80f25d8078b9ac4f9e8ca48a3ffcf
eynar-pari <eynar_repc@hotmail.com>
2020-06-05 08:13:20 -0400

    observador basic structure

---
+++ src/main/java/observer/basic/ISubject.java
@@ -0,0 +1,7 @@
+package observer.basic;
+
+public interface ISubject {
+    void attach(IObserver observer);
+    void deAttach(IObserver observer);
+    void notifyObserver();
+}
---
+++ src/main/java/observer/basic/ObservadorConcrete1.java
@@ -0,0 +1,8 @@
+package observer.basic;
+
+public class ObservadorConcrete1 implements IObserver{
+    @Override
+    public void update(String msg) {
+        System.out.println("ObservadorConcrete1  > [Notificacion]"+msg);
+    }
+}
