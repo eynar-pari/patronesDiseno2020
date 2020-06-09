09a9e73e3e381b4663fc3ffb7ffff45850fe75f4
eynar-pari <eynar_repc@hotmail.com>
2020-06-09 07:34:24 -0400

    ejemplo state

--- src/main/java/state/basic/Context.java
+++ src/main/java/state/basic/Context.java
@@ -1,7 +1,11 @@
 package state.basic;
 
 public class Context {
+    // agregar- cumpla patron state
     private IState state;
+
+    private String attribute1;
+    private int attribute2;
 
     public Context(){
     }
@@ -14,8 +18,24 @@ public class Context {
         this.state = state;
     }
 
+    // agregar- cumpla patron state
     public void request(){
         state.handle();
     }
 
+    public String getAttribute1() {
+        return attribute1;
+    }
+
+    public void setAttribute1(String attribute1) {
+        this.attribute1 = attribute1;
+    }
+
+    public int getAttribute2() {
+        return attribute2;
+    }
+
+    public void setAttribute2(int attribute2) {
+        this.attribute2 = attribute2;
+    }
 }
